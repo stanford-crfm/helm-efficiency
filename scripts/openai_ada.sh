022c81708be62a900ece4d16f72989c87fa31f84
Deepak Narayanan <deepakn94@gmail.com>
2022-03-12 23:40:34 +0000

    Update scripts file with fixed paths and copy in GPT-2 vocab files

--- scripts/openai_ada.sh
+++ scripts/openai_ada.sh
@@ -6,10 +6,12 @@ DISTRIBUTED_ARGS="--nproc_per_node 1 \
                   --master_addr localhost \
                   --master_port 6000"
 
-VOCAB_FILE=../data/megatron/data/gpt2/gpt2_vocab.json
-MERGE_FILE=../data/megatron/data/gpt2/gpt2_merges.txt
+VOCAB_FILE=data/gpt2/gpt2_vocab.json
+MERGE_FILE=data/gpt2/gpt2_merges.txt
 
-python -u -m torch.distributed.launch $DISTRIBUTED_ARGS tools/generate_text.py   \
+export PYTHONPATH=$PYTHONPATH:megatron_lm/
+
+python -u -m torch.distributed.launch $DISTRIBUTED_ARGS generate_text.py   \
        --tensor-model-parallel-size 1  \
        --pipeline-model-parallel-size 1  \
        --num-layers 24  \
@@ -25,7 +27,7 @@ python -u -m torch.distributed.launch $DISTRIBUTED_ARGS tools/generate_text.py  
        --vocab-file $VOCAB_FILE  \
        --merge-file $MERGE_FILE  \
        --top-p 0.9  \
-       --seed 42 \
-       --prompts-file example_text/alice_in_wonderland.txt \
-       --all-tokens-to-generate 0 1 2 4 8 12 16 24 32 48 64 \
+       --seed 42  \
+       --prompts-file data/alice_in_wonderland.txt  \
+       --all-num-output-tokens 0 1 2 4 8 12 16 24 32 48 64  \
        --all-num-input-tokens 1 16 256
