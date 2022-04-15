df333a172bd95d32d5e5be2dbbbc832f9ddeb418
Deepak Narayanan <deepakn94@gmail.com>
2022-04-15 21:13:45 +0000

    Sweep more input tokens (and increase --seq-length and --max-position-embeddings values)

--- scripts/ai21_j1_large.sh
+++ scripts/ai21_j1_large.sh
@@ -17,11 +17,11 @@ python -u -m torch.distributed.launch $DISTRIBUTED_ARGS generate_text.py   \
        --num-layers 32  \
        --hidden-size 4096  \
        --num-attention-heads 32  \
-       --max-position-embeddings 1024  \
+       --max-position-embeddings 2048  \
        --tokenizer-type GPT2BPETokenizer  \
        --fp16  \
        --micro-batch-size 1  \
-       --seq-length 1024  \
+       --seq-length 2048  \
        --temperature 1.0  \
        --vocab-file $VOCAB_FILE  \
        --merge-file $MERGE_FILE  \
@@ -29,4 +29,4 @@ python -u -m torch.distributed.launch $DISTRIBUTED_ARGS generate_text.py   \
        --seed 42  \
        --prompts-file data/alice_in_wonderland.txt  \
        --all-num-output-tokens 1 2 4 8 12 16 24 32 48 64  \
-       --all-num-input-tokens 1 16 32 64 128 192 256 320 384 428 472 512
+       --all-num-input-tokens 1 16 32 64 128 192 256 320 384 428 472 512 576 640 704 768 832 896 960 1024 1152 1280 1408 1536 1664 1792 1920 2048
