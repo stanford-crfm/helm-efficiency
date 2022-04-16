a41d3e000fde60b23e2996b25a9efb8914d2500d
Deepak Narayanan <deepakn94@gmail.com>
2022-04-15 17:19:53 -0700

    Add script to profile Megatron NLG model

--- scripts/run_all.sh
+++ scripts/run_all.sh
@@ -5,3 +5,5 @@ bash scripts/openai_davinci.sh | tee logs/$HARDWARE/openai_davinci.log
 bash scripts/ai21_j1_large.sh | tee logs/$HARDWARE/ai21_j1_large.log
 bash scripts/ai21_j1_jumbo.sh | tee logs/$HARDWARE/ai21_j1_jumbo.log
 bash scripts/gptj_6b.sh | tee logs/$HARDWARE/gptj_6b.log
+bash scripts/gpt2.sh | tee logs/$HARDWARE/gpt2.log
+bash scripts/mt_nlg.sh | tee logs/$HARDWARE/mt_nlg.log
