34fff53e1380a9e559cb3045e3b7b58f3b77fdea
Deepak Narayanan <deepakn94@gmail.com>
2022-04-19 01:24:39 +0000

    Anthropic profiling script

--- scripts/run_all.sh
+++ scripts/run_all.sh
@@ -2,6 +2,7 @@ HARDWARE=v100
 
 bash scripts/gpt2.sh | tee logs/$HARDWARE/gpt2.log
 bash scripts/mt_nlg.sh | tee logs/$HARDWARE/mt_nlg.log
+bash scripts/anthropic.sh | tee logs/$HARDWARE/anthropic.log
 bash scripts/openai_ada.sh | tee logs/$HARDWARE/openai_ada.log
 bash scripts/openai_davinci.sh | tee logs/$HARDWARE/openai_davinci.log
 bash scripts/ai21_j1_large.sh | tee logs/$HARDWARE/ai21_j1_large.log
