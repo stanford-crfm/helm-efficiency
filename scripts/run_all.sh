f4a11d3f9bbc75e14286051da659b2c620e1706d
Deepak Narayanan <deepakn94@gmail.com>
2022-04-19 14:58:23 -0700

    GPT NeoX scripts

--- scripts/run_all.sh
+++ scripts/run_all.sh
@@ -8,3 +8,4 @@ bash scripts/openai_davinci.sh | tee logs/$HARDWARE/openai_davinci.log
 bash scripts/ai21_j1_large.sh | tee logs/$HARDWARE/ai21_j1_large.log
 bash scripts/ai21_j1_jumbo.sh | tee logs/$HARDWARE/ai21_j1_jumbo.log
 bash scripts/gptj_6b.sh | tee logs/$HARDWARE/gptj_6b.log
+bash scripts/gpt_neox_20b.sh | tee logs/$HARDWARE/gpt_neox_20b.log
