b77760064440563f41b35775a99d7b2c861bfea2
Deepak Narayanan <deepakn94@gmail.com>
2022-08-17 17:15:31 +0000

    Script files for new models

---
+++ scripts/opt_66b.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+DISTRIBUTED_ARGS="--nproc_per_node 4 \
+                  --nnodes 1 \
+                  --node_rank 0 \
+                  --master_addr localhost \
+                  --master_port 6000"
+
+VOCAB_FILE=data/gpt2/gpt2_vocab.json
+MERGE_FILE=data/gpt2/gpt2_merges.txt
+
+export PYTHONPATH=$PYTHONPATH:megatron_lm/
+
+python -u -m torch.distributed.launch $DISTRIBUTED_ARGS generate_text.py   \
+       --tensor-model-parallel-size 4  \
+       --pipeline-model-parallel-size 1  \
+       --num-layers 64  \
+       --hidden-size 9216  \
+       --num-attention-heads 72  \
+       --max-position-embeddings 2048  \
+       --tokenizer-type GPT2BPETokenizer  \
+       --fp16  \
+       --micro-batch-size 1  \
+       --seq-length 2048  \
+       --temperature 1.0  \
+       --vocab-file $VOCAB_FILE  \
+       --merge-file $MERGE_FILE  \
+       --top-p 0.9  \
+       --seed 42  \
+       --prompts-file data/alice_in_wonderland.txt  \
+       --all-num-output-tokens 1 2 4 8 12 16 24 32 48 64  \
+       --all-num-input-tokens 1 16 32 64 128 192 256 320 384 428 472 512 576 640 704 768 832 896 960 1024 1152 1280 1408 1536 1664 1792 1920 2048
--- scripts/run_all.sh
+++ scripts/run_all.sh
@@ -1,4 +1,4 @@
-HARDWARE=v100
+HARDWARE=a100
 
 bash scripts/gpt2.sh | tee logs/$HARDWARE/gpt2.log
 bash scripts/mt_nlg.sh | tee logs/$HARDWARE/mt_nlg.log
@@ -9,3 +9,6 @@ bash scripts/ai21_j1_large.sh | tee logs/$HARDWARE/ai21_j1_large.log
 bash scripts/ai21_j1_jumbo.sh | tee logs/$HARDWARE/ai21_j1_jumbo.log
 bash scripts/gptj_6b.sh | tee logs/$HARDWARE/gptj_6b.log
 bash scripts/gpt_neox_20b.sh | tee logs/$HARDWARE/gpt_neox_20b.log
+bash scripts/bloom.sh | tee logs/$HARDWARE/bloom.log
+bash scripts/yalm.sh | tee logs/$HARDWARE/yalm.log
+bash scripts/opt_66b.sh | tee logs/$HARDWARE/opt_66b.log
