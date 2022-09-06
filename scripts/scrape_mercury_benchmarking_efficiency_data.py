5bdef90a3fee887984e7a3ce79e53f61eae3bc7a
Deepak Narayanan <deepakn94@gmail.com>
2022-09-06 09:54:37 -0700

    Add a way to download per_instance_metrics.json files from CRFM website (#7)

--- scripts/scrape_mercury_benchmarking_efficiency_data.py
+++ scripts/scrape_mercury_benchmarking_efficiency_data.py
@@ -2,12 +2,13 @@ import argparse
 from bs4 import BeautifulSoup
 import requests
 import json
+import os
 import pandas as pd
 from selenium import webdriver
 import time
 import tqdm
 
-BASE_URL = "https://crfm-models.stanford.edu/static/benchmarking.html"
+BASE_URL = "https://crfm-models.stanford.edu/static/benchmarking.html?runs"
 DELAY = 60
 
 
@@ -31,12 +32,12 @@ def parse(runs, keys):
     all_data = []
 
     for run in tqdm.tqdm(runs):
-        url = f"https://crfm-models.stanford.edu/static/benchmark_output/runs/latest/{run}/metrics.json"
+        url = f"https://crfm-models.stanford.edu/static/benchmark_output/runs/latest/{run}/stats.json"
         r = requests.get(url)
         try:
             data = {}
-            metrics = json.loads(r.text)
-            for metric in metrics:
+            stats = json.loads(r.text)
+            for metric in stats:
                 name = metric["name"]["name"]
                 if name in keys and (name != "exact_match" or metric["name"]["k"] == 1):
                     if len(metric["values"]) == 0:
@@ -48,16 +49,30 @@ def parse(runs, keys):
                     data[key] = None
             all_data.append((run, *[data[key] for key in keys]))
         except Exception as e:
-            print(f"Could not parse metrics for run {run} at {url}")
-            print(r.text)
+            print(f"Could not parse stats for run {run} at {url}")
             continue
 
     return all_data
 
 
+def save_per_instance_stats(runs):
+    for run in tqdm.tqdm(runs):
+        url = f"https://crfm-models.stanford.edu/static/benchmark_output/runs/latest/{run}/per_instance_stats.json"
+        r = requests.get(url)
+        try:
+            stats = json.loads(r.text)
+            directory = f"benchmark_output/runs/latest/{run}"
+            os.makedirs(directory, exist_ok=True)
+            with open(f"{directory}/per_instance_stats.json", 'w') as f:
+                f.write(r.text)
+        except Exception as e:
+            print(f"Could not parse stats for run {run} at {url}")
+            continue
+
+
 def save(data, keys, output_file):
     df = pd.DataFrame(data, columns=["run", *keys])
-    df.to_csv(output_file, sep="\t", index=False)
+    df.to_csv(output_file, sep=",", index=False)
 
 
 def main(args):
@@ -75,14 +90,19 @@ def main(args):
     ]
 
     runs = scrape()
-    data = parse(runs, keys)
-    save(data, keys, args.output_file)
+    if args.save_aggregated_stats:
+        data = parse(runs, keys)
+        save(data, keys, args.output_file)
+    if save_per_instance_stats:
+        save_per_instance_stats(runs)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Scrape CRFM benchmarking results")
     parser.add_argument(
-        "--output_file", type=str, default="mercury_benchmarking_inference_data.tsv"
+        "--output_file", type=str, default="mercury_benchmarking_inference_data.csv"
     )
+    parser.add_argument("--save-aggregated-stats", action='store_true')
+    parser.add_argument("--save-per-instance-stats", action='store_true')
     args = parser.parse_args()
     main(args)
