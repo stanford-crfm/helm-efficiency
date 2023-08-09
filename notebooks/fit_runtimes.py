5daf1aa4fd415c2ddc59432bbbd497bc80b56d6b
Deepak Narayanan <deepakn94@gmail.com>
2023-08-08 17:12:03 -0700

    Compute fits with random runtimes as well

--- notebooks/fit_runtimes.py
+++ notebooks/fit_runtimes.py
@@ -76,21 +76,13 @@ def compute_best_fit(runtimes, models,
             for label, runtime in runtimes.items():
                 if label[0] == model and label[1] == num_prompt_tokens:
                     num_output_tokens_and_runtimes.append(
-                        (label[2], runtime - runtime_for_prompt_tokens[
+                        (label[2] - 1, runtime - runtime_for_prompt_tokens[
                             num_prompt_tokens]))
         runtime_per_output_token, _ = solve_linear_regression(
             num_output_tokens_and_runtimes, print_r_squared=True)
-        overhead = round(runtime_for_prompt_tokens[all_num_prompt_tokens[0]]
-                         - runtime_per_output_token, 3)
-        if real_system:
-            for num_prompt_tokens in runtime_for_prompt_tokens:
-                runtime_for_prompt_tokens[num_prompt_tokens] -= overhead
-                runtime_for_prompt_tokens[num_prompt_tokens] = round(
-                    runtime_for_prompt_tokens[num_prompt_tokens], 3)
 
         print(f"{model}:\n\tRuntime per output token = "
               f"{runtime_per_output_token:.3f} seconds")
-        print(f"\tOverhead: {overhead:.3f} seconds")
 
         for i, num_prompt_tokens in enumerate(all_num_prompt_tokens):
             runtime = runtime_for_prompt_tokens[num_prompt_tokens]
@@ -106,14 +98,12 @@ def compute_best_fit(runtimes, models,
                         num_prompt_tokens] + (
                         (label[2] - 1) * runtime_per_output_token)
                     square_errors.append((runtime - estimated_runtime)**2)
-        print(f"\tMean squared error: {np.mean(square_errors):.4f}")
-        print(f"\tMaximum squared error: {max(square_errors):.4f}")
+        print(f"Mean squared error: {np.mean(square_errors):.4f}")
+        print(f"Maximum squared error: {max(square_errors):.4f}")
         print()
 
         model_obj['runtime_per_output_token'] = runtime_per_output_token
         model_obj['runtime_for_prompt_tokens'] = runtime_for_prompt_tokens
-        if real_system:
-            model_obj['overhead'] = overhead
                 
         if model in name_mapping:
             model_names = name_mapping[model]
